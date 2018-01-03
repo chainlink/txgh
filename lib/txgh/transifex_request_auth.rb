617881a2956e2080ca57e10f66e97e6eecaa8f6b
Kyle White <kylewhite@gmail.com>
2018-01-03 16:58:05 -0500

    Change to work with v2 auth

--- lib/txgh/transifex_request_auth.rb
+++ lib/txgh/transifex_request_auth.rb
@@ -2,56 +2,33 @@ require 'openssl'
 
 module Txgh
   class TransifexRequestAuth
-    HMAC_DIGEST = OpenSSL::Digest.new('sha1')
-    RACK_HEADER = 'HTTP_X_TX_SIGNATURE'
-    TRANSIFEX_HEADER = 'X-TX-Signature'
+    HMAC_DIGEST_256 = OpenSSL::Digest.new('sha256')
+    TRANSIFEX_HEADER = 'HTTP_X_TX_SIGNATURE_V2' #'X-TX-Signature-V2'
+    DATE_HEADER = 'HTTP_DATE'
+    URL_HEADER = 'HTTP_X_TX_URL'
 
     class << self
       def authentic_request?(request, secret)
         request.body.rewind
         content = request.body.read
-        if request.env['CONTENT_TYPE'] == "application/json"
-          params = JSON.parse(content)
-        else
-          params = URI.decode_www_form(content)
-        end
 
-        expected_signature = header_value(params, secret)
-        actual_signature = request.env[RACK_HEADER]
+        http_verb = 'POST' #TODO Use verb from request
+        http_date = request.env[DATE_HEADER]
+        url = request.env[URL_HEADER]
+        content_md5 = Digest::MD5.hexdigest content
+
+        data = [http_verb, url, http_date, content_md5].join("\n")
+        expected_signature = digest(data, secret)
+        actual_signature = request.env[TRANSIFEX_HEADER]
         actual_signature == expected_signature
       end
 
-      def header_value(content, secret)
-        digest(transform(content), secret)
-      end
 
       private
 
-      # In order to generate a correct HMAC hash, the request body must be
-      # parsed and made to look like a python map. If you're thinking that's
-      # weird, you're correct, but it's apparently expected behavior.
-      def transform(params)
-        params = params.map do |key, val|
-          key = "'#{key}'"
-          val = interpret_val(val)
-          "#{key}: #{val}"
-        end
-
-        "{#{params.join(', ')}}"
-      end
-
-      def interpret_val(val)
-        val = "#{val}"
-        if val =~ /\A[\d]+\z/
-          val
-        else
-          "u'#{val}'"
-        end
-      end
-
-      def digest(content, secret)
+      def digest(data, secret)
         Base64.encode64(
-          OpenSSL::HMAC.digest(HMAC_DIGEST, secret, content)
+          OpenSSL::HMAC.digest(HMAC_DIGEST_256, secret, data)
         ).strip
       end
     end
